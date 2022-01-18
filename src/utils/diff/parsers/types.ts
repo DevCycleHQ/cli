a25865af648c15b0d53d9eba0319fd1abebb4c87
Adam Wootton <adam@taplytics.com>
2022-01-18 11:14:55 -0500

    switch to use abstract class base parser

--- src/utils/diff/parsers/types.ts
+++ src/utils/diff/parsers/types.ts
@@ -1,13 +1,6 @@
-import parse from 'parse-diff'
-
 export type VariableMatch = {
     name: string,
     line: number,
     fileName: string,
     mode: 'add' | 'remove'
-}
-
-export type Parser = {
-    parse: (files: parse.File) => VariableMatch[],
-    identity: string
 }
