cbf19b1a3be2982a3d9e4cf0bc3303bddbe374e7
Adam Wootton <adam@taplytics.com>
2022-01-24 12:52:04 -0500

    add support for named arguments in languages that have them

--- src/utils/diff/parsers/android/index.ts
+++ src/utils/diff/parsers/android/index.ts
@@ -3,6 +3,7 @@ import { BaseParser } from '../common'
 export class AndroidParser extends BaseParser {
     identity = 'android'
     variableMethodPattern = /\.variable\(\s*/
+    variableMethodKeywordPattern = /\.variable\(\s*key\s*=\s*/
     variableNameCapturePattern = /["']([^"']*)["']/
     commentCharacters = ['//', '/**', '*', '<!--']
 
--- src/utils/diff/parsers/python/index.ts
+++ src/utils/diff/parsers/python/index.ts
@@ -3,6 +3,7 @@ import { BaseParser } from '../common'
 export class PythonParser extends BaseParser {
     identity = 'python'
     variableMethodPattern = /\.variable\([\s\w]*,\s*/
+    variableMethodKeywordPattern = /\.variable\([\s\w=,]*key\s*=\s*/
     variableNameCapturePattern = /["']([^"']*)["']/
     commentCharacters = ['#', '"""']
 
