f0512bd1e4f9cc93305e54b50437c0bb8a5efa18
Adam Wootton <adam@taplytics.com>
2022-01-24 12:52:04 -0500

    add support for named arguments in languages that have them

--- src/utils/diff/parsers/php/index.ts
+++ src/utils/diff/parsers/php/index.ts
@@ -3,6 +3,7 @@ import { BaseParser } from '../common'
 export class PhpParser extends BaseParser {
     identity = 'php'
     variableMethodPattern = /->variable\([\s\w$]*,\s*/
+    variableMethodKeywordPattern = /->variable\([\s\w$:,]*key\s*:\s*/
     variableNameCapturePattern = /["']([^"']*)["']/
     commentCharacters = ['//', '#', '/*']
 
