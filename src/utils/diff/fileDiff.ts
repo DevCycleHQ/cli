5a120fee96704d68b2f85437885db704a13254ed
Laura Warr <laura.warr@taplytics.com>
2022-01-24 09:20:51 -0800

    Use cursorPosition to get all matches

--- src/utils/diff/fileDiff.ts
+++ src/utils/diff/fileDiff.ts
@@ -4,7 +4,5 @@ import * as fs from 'node:fs'
 export const executeFileDiff = (filePath: string): parse.File[] => {
     const result = fs.readFileSync(filePath, 'utf8')
 
-    const parsed = parse(result)
-
-    return parsed
+    return parse(result)
 }
