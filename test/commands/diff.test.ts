92726fc749fa1c44e904e7679569773ee79e4f3f
Adam Wootton <adam@taplytics.com>
2022-01-27 12:34:38 -0500

    add ability to linkify diff outputs

--- test/commands/diff.test.ts
+++ test/commands/diff.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { expect, test } from '@oclif/test'
 import { AUTH_URL } from '../../src/api/common'
 
@@ -9,20 +10,20 @@ DevCycle Variable Changes:
 
 ✅ Added
 
-	1. simple-case
+  1. simple-case
 	   Locations:
 	    - test/utils/diff/sampleDiff.js:L1
 	    - test/utils/diff/sampleDiff.js:L2
-	2. single-quotes
+  2. single-quotes
 	   Location: test/utils/diff/sampleDiff.js:L4
-	3. multi-line
+  3. multi-line
 	   Location: test/utils/diff/sampleDiff.js:L10
-	4. multi-line-comment
+  4. multi-line-comment
 	   Location: test/utils/diff/sampleDiff.js:L20
 
 ❌ Removed
 
-	1. simple-case
+  1. simple-case
 	   Location: test/utils/diff/sampleDiff.js:L1
 `
 
@@ -34,23 +35,48 @@ DevCycle Variable Changes:
 
 ✅ Added
 
-	1. simple-case
+  1. simple-case
 	   Locations:
 	    - test/utils/diff/sampleDiff.js:L1
 	    - test/utils/diff/sampleDiff.js:L2
-	2. single-quotes
+  2. single-quotes
 	   Location: test/utils/diff/sampleDiff.js:L4
-	3. multi-line
+  3. multi-line
 	   Location: test/utils/diff/sampleDiff.js:L10
-	4. multi-line-comment
+  4. multi-line-comment
 	   Location: test/utils/diff/sampleDiff.js:L20
-	5. func-proxy
+  5. func-proxy
 	   Location: test/utils/diff/sampleDiff.js:L6
 
 ❌ Removed
 
-	1. simple-case
+  1. simple-case
 	   Location: test/utils/diff/sampleDiff.js:L1
+`
+
+const linkedExpected = `
+DevCycle Variable Changes:
+
+✅ 4 Variables Added
+❌ 1 Variable Removed
+
+✅ Added
+
+  1. simple-case
+	   Locations:
+	    - [test/utils/diff/sampleDiff.js:L1](https://example.com/files#diff-c197a837fe3ee51fcef381dc90df1cde5c759ad43f47c0cb72968af943205fa3R1)
+	    - [test/utils/diff/sampleDiff.js:L2](https://example.com/files#diff-c197a837fe3ee51fcef381dc90df1cde5c759ad43f47c0cb72968af943205fa3R2)
+  2. single-quotes
+	   Location: [test/utils/diff/sampleDiff.js:L4](https://example.com/files#diff-c197a837fe3ee51fcef381dc90df1cde5c759ad43f47c0cb72968af943205fa3R4)
+  3. multi-line
+	   Location: [test/utils/diff/sampleDiff.js:L10](https://example.com/files#diff-c197a837fe3ee51fcef381dc90df1cde5c759ad43f47c0cb72968af943205fa3R10)
+  4. multi-line-comment
+	   Location: [test/utils/diff/sampleDiff.js:L20](https://example.com/files#diff-c197a837fe3ee51fcef381dc90df1cde5c759ad43f47c0cb72968af943205fa3R20)
+
+❌ Removed
+
+  1. simple-case
+	   Location: [test/utils/diff/sampleDiff.js:L1](https://example.com/files#diff-c197a837fe3ee51fcef381dc90df1cde5c759ad43f47c0cb72968af943205fa3L1)
 `
 
 describe('diff', () => {
@@ -114,4 +140,10 @@ describe('diff', () => {
             '--client-id', 'client', '--client-secret', 'secret', '--project', 'project'])
         .catch('Failed to authenticate with the DevCycle API. Check your credentials.')
         .it('runs with failed api authorization')
+    test
+        .stdout()
+        .command(['diff', '--file', './test/utils/diff/samples/nodejs', '--no-api', '--pr-link', 'https://example.com'])
+        .it('runs against a test file and linkifies the output', (ctx) => {
+            expect(ctx.stdout).to.equal(linkedExpected)
+        })
 })
