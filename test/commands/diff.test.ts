929683e9dab5b78c57ba56e37c83e5466be3f5c3
Adam Wootton <adam@taplytics.com>
2022-01-31 12:56:14 -0500

    add API data enrichment to diff command

--- test/commands/diff.test.ts
+++ test/commands/diff.test.ts
@@ -1,12 +1,14 @@
 /* eslint-disable max-len */
 import { expect, test } from '@oclif/test'
-import { AUTH_URL } from '../../src/api/common'
+import { AUTH_URL, BASE_URL } from '../../src/api/common'
+
+process.env = {}
 
 const expected = `
 DevCycle Variable Changes:
 
-✅ 4 Variables Added
-❌ 1 Variable Removed
+✅  4 Variables Added
+❌  1 Variable Removed
 
 ✅ Added
 
@@ -30,8 +32,8 @@ DevCycle Variable Changes:
 const customExpected = `
 DevCycle Variable Changes:
 
-✅ 5 Variables Added
-❌ 1 Variable Removed
+✅  5 Variables Added
+❌  1 Variable Removed
 
 ✅ Added
 
@@ -57,8 +59,8 @@ DevCycle Variable Changes:
 const linkedExpected = `
 DevCycle Variable Changes:
 
-✅ 4 Variables Added
-❌ 1 Variable Removed
+✅  4 Variables Added
+❌  1 Variable Removed
 
 ✅ Added
 
@@ -77,6 +79,41 @@ DevCycle Variable Changes:
 
   1. simple-case
 	   Location: [test/utils/diff/sampleDiff.js:L1](https://example.com/files#diff-c197a837fe3ee51fcef381dc90df1cde5c759ad43f47c0cb72968af943205fa3L1)
+`
+
+const apiExpected = `
+DevCycle Variable Changes:
+
+⚠️   1 Variable With Notices
+✅  2 Variables Added
+❌  2 Variables Removed
+🧹  1 Variable Cleaned up
+
+⚠️  Notices
+
+  1. Variable "no-exists" does not exist on DevCycle
+
+✅ Added
+
+  1. exists
+	   Type: String
+	   Location: test/utils/diff/sampleDiff.js:L1
+  2. no-exists ⚠️
+	   Location: test/utils/diff/sampleDiff.js:L2
+
+❌ Removed
+
+  1. exists2
+	   Type: String
+	   Location: test/utils/diff/sampleDiff.js:L1
+  2. no-exists2 🧹
+	   Location: test/utils/diff/sampleDiff.js:L2
+
+🧹 Cleaned Up
+
+The following variables that do not exist in DevCycle were cleaned up:
+
+  1. no-exists2
 `
 
 describe('diff', () => {
@@ -105,25 +142,7 @@ describe('diff', () => {
             (ctx) => {
                 expect(ctx.stdout).to.equal(customExpected)
             })
-    test
-        .nock(AUTH_URL, (api) => {
-            api.post('/oauth/token', {
-                grant_type: 'client_credentials',
-                client_id: 'client',
-                client_secret: 'secret',
-                audience: 'https://api.devcycle.com/',
-            }).reply(200, {
-                access_token: 'token'
-            })
-        })
-        .stdout()
-        .command(['diff', '--file',
-            './test/utils/diff/samples/nodejs',
-            '--client-id', 'client', '--client-secret', 'secret', '--project', 'project'])
-        .it('runs with successful api authorization',
-            (ctx) => {
-                expect(ctx.stdout).to.equal(expected)
-            })
+
     test
         .nock(AUTH_URL, (api) => {
             api.post('/oauth/token', {
@@ -140,10 +159,45 @@ describe('diff', () => {
             '--client-id', 'client', '--client-secret', 'secret', '--project', 'project'])
         .catch('Failed to authenticate with the DevCycle API. Check your credentials.')
         .it('runs with failed api authorization')
+
     test
         .stdout()
         .command(['diff', '--file', './test/utils/diff/samples/nodejs', '--no-api', '--pr-link', 'https://example.com'])
         .it('runs against a test file and linkifies the output', (ctx) => {
             expect(ctx.stdout).to.equal(linkedExpected)
+        })
+
+    test
+        .nock(AUTH_URL, (api) => {
+            api.post('/oauth/token', {
+                grant_type: 'client_credentials',
+                client_id: 'client',
+                client_secret: 'secret',
+                audience: 'https://api.devcycle.com/',
+            }).reply(200, {
+                access_token: 'token'
+            })
+        })
+        .nock(BASE_URL, { reqheaders: { authorization: 'token' } },(api) => {
+            api.get(/v1\/projects\/project\/variables\/.*/)
+                .times(4)
+                .reply(200, (uri) => {
+                    const parts = uri.split('/')
+                    const key = parts[parts.length - 1]
+                    if (key.includes('no-exists')) {
+                        return null
+                    }
+
+                    return {
+                        key,
+                        type: 'String'
+                    }
+                })
+        })
+        .stdout()
+        .command(['diff', '--file', './test/utils/diff/samples/apiEnrichment/enrichment',
+            '--client-id', 'client', '--client-secret', 'secret', '--project', 'project'])
+        .it('enriches output with API data', (ctx) => {
+            expect(ctx.stdout).to.equal(apiExpected)
         })
 })
