7a320048c64b4a502c5b0b2222ce4f411ec775ff
Laura Warr <laura.warr@taplytics.com>
2022-03-01 10:23:17 -0800

    Add include/exclude params to control which files are scanned

--- src/commands/usages/index.ts
+++ src/commands/usages/index.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import minimatch from 'minimatch'
 import { lsFiles } from '../../utils/git/ls-files'
 import { Flags } from '@oclif/core'
 import { parseFiles } from '../../utils/usages/parse'
@@ -7,9 +8,8 @@ import { File, LineItem } from './types'
 
 export default class Usages extends Base {
     static hidden = false
-    authSuggested = true
 
-    static description = 'Print all DevCycle variable usage in the current version of your code.'
+    static description = 'Print all DevCycle variable usages in the current version of your code.'
     static examples = [
         '<%= config.bin %> <%= command.id %>',
         '<%= config.bin %> <%= command.id %> ' +
@@ -18,6 +18,16 @@ export default class Usages extends Base {
 
     static flags = {
         ...Base.flags,
+        'include': Flags.string({
+            description: 'Files to include when scanning for usages. By default all files are included. ' +
+                'Accepts multiple glob patterns.',
+            multiple: true
+        }),
+        'exclude': Flags.string({
+            description: 'Files to exclude when scanning for usages. By default all files are included. ' +
+                'Accepts multiple glob patterns.',
+            multiple: true
+        }),
         'client-name': Flags.string({
             description: 'Name(s) of the DevCycle client variable to match on. Accepts multiple values.', multiple: true
         }),
@@ -43,35 +53,52 @@ export default class Usages extends Base {
         })
     }
 
-    static args = [
-        { name: 'diff-pattern', description: 'A "git diff"-compatible diff pattern, eg. "branch1 branch2"' },
-    ]
-
     useMarkdown = false
 
     public async run(): Promise<void> {
         const { flags } = await this.parse(Usages)
+        const codeInsightsConfig = this.configFromFile?.codeInsights || {}
 
         this.useMarkdown = flags.format === 'markdown'
 
-        const files = lsFiles().map((filename): File => {
+        const includeFile = (filepath: string) => {
+            const includeGlobs = flags['include'] || codeInsightsConfig.includeFiles
+            return includeGlobs
+                ? includeGlobs.some((glob) => minimatch(filepath, glob, { matchBase: true }))
+                : true
+        }
+
+        const excludeFile = (filepath: string) => {
+            const excludeGlobs = flags['exclude'] || codeInsightsConfig.excludeFiles
+            return excludeGlobs
+                ? excludeGlobs.some((glob) => minimatch(filepath, glob, { matchBase: true }))
+                : false
+        }
+
+        const processFile = (filepath: string): File => {
             let lines: LineItem[] = []
             try {
                 lines = fs
-                    .readFileSync(filename, 'utf8')
+                    .readFileSync(filepath, 'utf8')
                     .split('\n')
                     .map((content, ln) => ({ content, ln }))
             } catch (err) {
-                this.warn(`Error parsing file ${filename}`)
+                this.warn(`Error parsing file ${filepath}`)
                 this.debug(err)
             }
             return {
-                name: filename,
+                name: filepath,
                 lines
             }
-        })
+        } 
 
-        const codeInsightsConfig = this.configFromFile?.codeInsights || {}
+        const files = lsFiles()
+            .filter((filepath) => includeFile(filepath) && !excludeFile(filepath))
+            .map(processFile)
+
+        if (!files.length) {
+            this.warn('No files found to process.')
+        }
 
         const matchPatternsFromConfig: Record<string, string[]> = codeInsightsConfig.matchPatterns || {}
         const clientNamesFromConfig = codeInsightsConfig.clientNames || []
