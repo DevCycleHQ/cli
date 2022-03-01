6df99c72265ae88d2b80b73c90e75eb07ac46340
Laura Warr <laura.warr@taplytics.com>
2022-03-01 15:50:32 -0800

    Relocate parser tests

--- test/utils/parsers/python.test.ts
+++ test/utils/parsers/python.test.ts
@@ -1,6 +1,6 @@
-import { executeFileDiff } from '../../../../src/utils/diff/fileDiff'
+import { executeFileDiff } from '../../../src/utils/diff/fileDiff'
 import * as path from 'node:path'
-import { parseFiles } from '../../../../src/utils/diff/parse'
+import { parseFiles } from '../../../src/utils/diff/parse'
 import { expect } from '@oclif/test'
 
 describe('python', () => {
@@ -38,7 +38,7 @@ describe('python', () => {
     ]
 
     it('identifies the correct variable usages in the Python sample diff', () => {
-        const parsedDiff = executeFileDiff(path.join(__dirname, '../samples/python'))
+        const parsedDiff = executeFileDiff(path.join(__dirname, '../diff/samples/python'))
         const results = parseFiles(parsedDiff)
 
         expect(results).to.deep.equal({
