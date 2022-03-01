48a587505b6647da7d5acc817cbdeecbcb17f644
Laura Warr <laura.warr@taplytics.com>
2022-03-01 10:59:59 -0800

    Reduce duplication for shared command flags

---
+++ src/flags/match-pattern/index.ts
@@ -0,0 +1,21 @@
+import { Flags } from '@oclif/core'
+import { ConfigFromFile } from '../../types'
+
+export default Flags.string({
+    description: 'Additional full Regex pattern to use to match variable usages in your code.' +
+        ' Should contain exactly one capture group which matches on the key of the variable. ' +
+        'Must specify the file extension to override the pattern for, eg. "--match-pattern js=<YOUR PATTERN>"',
+    multiple: true
+})
+
+export function getMatchPatterns(flags: Record<string, any>, config: ConfigFromFile | null) {
+    const matchPatternsFromConfig = config?.codeInsights?.matchPatterns || {}
+
+    return (flags['match-pattern'] || []).reduce((map: Record<string, string>, value: string) => {
+        const [extension, pattern] = value.split('=')
+        if (!extension || !pattern) {
+            throw new Error(`Invalid match pattern: ${value}. Must be of the form "[FILE EXTENSION]=[PATTERN]`)
+        }
+        return { ...map, [extension]: [...(map[extension] || []), pattern] }
+    }, matchPatternsFromConfig)
+}
---
+++ src/flags/var-alias/index.ts
@@ -0,0 +1,21 @@
+import { Flags } from '@oclif/core'
+import { ConfigFromFile } from '../../types'
+
+export default Flags.string({
+    description: 'Aliases to use when identifying variables in your code.' +
+    ' Should contain a code reference mapped to a DevCycle variable key,' +
+    ' eg. "--var-alias "VARIABLES.ENABLE_V1=enable-v1"',
+    multiple: true
+})
+
+export function getVariableAliases(flags: Record<string, any>, config: ConfigFromFile | null) {
+    const variableAliasesFromConfig = config?.codeInsights?.variableAliases || {}
+
+    return (flags['var-alias'] || []).reduce((map: Record<string, string>, value: string) => {
+        const [codeRef, variableName] = value.trim().split('=')
+        if (!codeRef || !variableName) {
+            throw new Error(`Invalid variable alias: ${value}. Must be of the form "[CODE REF]=[VARIABLE KEY]`)
+        }
+        return { ...map, [codeRef]: variableName }
+    }, variableAliasesFromConfig)
+}
