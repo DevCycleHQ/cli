afd8469c8f6107a265da7e4ef2eff101e3eff7be
Andrew Traviss <traviss@taplytics.com>
2022-03-04 17:23:02 -0500

    Add variable creation

---
+++ src/api/features.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { BASE_URL } from './common'
+export class Feature {
+    _id: string
+    _project?: string
+    name?: string
+    description?: string
+    key: string
+    type: 'release'|'experiment'|'permission'|'ops'
+    tags: string[]
+    createdAt: Date
+    updatedAt: Date
+}
+
+export const fetchFeatures = async (token: string, project_id: string): Promise<Feature[]> => {
+    const url = new URL(`/v1/projects/${project_id}/features`, BASE_URL)
+    const response = await axios.get(url.href, {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+        },
+    })
+
+    return response.data
+}
+
+export const fetchFeatureByKey = async (token: string, project_id: string, key: string): Promise<Feature | null> => {
+    const url = new URL(`/v1/projects/${project_id}/features/${key}`, BASE_URL)
+    try {
+        const response = await axios.get(url.href, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: token,
+            },
+        })
+
+        return response.data
+    } catch (e: any) {
+        if (e.response?.status === 404) {
+            return null
+        }
+        throw e
+    }
+
+}
