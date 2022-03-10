47520937931c9ffffe7036dbe2ec568729cc67a6
Laura Warr <laura.warr@taplytics.com>
2022-03-10 13:40:02 -0800

    Move parser tests back to diff directory

--- test/utils/diff/parsers/android.test.ts
+++ test/utils/diff/parsers/android.test.ts
@@ -1,6 +1,6 @@
-import { executeFileDiff } from '../../../src/utils/diff/fileDiff'
+import { executeFileDiff } from '../../../../src/utils/diff/fileDiff'
 import * as path from 'node:path'
-import { parseFiles } from '../../../src/utils/diff/parse'
+import { parseFiles } from '../../../../src/utils/diff/parse'
 import { expect } from '@oclif/test'
 
 describe('android', () => {
