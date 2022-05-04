3ee2cab3c4e9658390375187a3834340887e183d
Elliot <elliot@taplytics.com>
2022-05-04 14:25:05 -0400

    fix quotes

--- src/utils/diff/diff.ts
+++ src/utils/diff/diff.ts
@@ -5,6 +5,6 @@ import { readFileSync } from 'fs'
 export const executeDiff = (diffCommand: string): parse.File[] => {
     execSync(`git diff ${diffCommand} > diff.txt`, { stdio: 'ignore' })
     const result = readFileSync('diff.txt', 'utf8')
-    execSync(`rm diff.txt`, { stdio: 'ignore' })
+    execSync('rm diff.txt', { stdio: 'ignore' })
     return parse(result)
 }
