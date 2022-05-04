56884a22734072c58d625716b99550fb23c44291
Elliot <elliot@taplytics.com>
2022-05-04 14:18:52 -0400

    pipe diff into file and then read from it

--- src/utils/diff/diff.ts
+++ src/utils/diff/diff.ts
@@ -1,8 +1,10 @@
 import { execSync } from 'node:child_process'
 import parse from 'parse-diff'
+import { readFileSync } from 'fs'
 
 export const executeDiff = (diffCommand: string): parse.File[] => {
-    const result = execSync(`git diff ${diffCommand}`)
-
-    return parse(result.toString())
+    execSync(`git diff ${diffCommand} > diff.txt`, { stdio: 'ignore' })
+    const result = readFileSync('diff.txt', 'utf8')
+    execSync(`rm diff.txt`, { stdio: 'ignore' })
+    return parse(result)
 }
