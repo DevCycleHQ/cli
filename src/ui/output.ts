001b5dab7fdaaae4108bbbd1ed5d3248f533d5a5
Andrew Traviss <traviss@taplytics.com>
2022-06-09 09:39:36 -0400

    fixed simple linting error

--- src/ui/output.ts
+++ src/ui/output.ts
@@ -4,6 +4,6 @@ export function successMessage(message:string):void {
     console.log(chalk.green(`✅ ${message}`))
 }
 
-export function showResults(results:unknown) {
+export function showResults(results:unknown):void {
     console.log(JSON.stringify(results, null, 2))
 }
