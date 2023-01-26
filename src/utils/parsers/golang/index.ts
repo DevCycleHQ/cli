18d386e86da017d26472a80aebe1b9934d55f930
Jason Salaber <jcsalaber@hotmail.com>
2023-01-26 14:53:41 -0500

    chore: update cli to capture go changes to sdk

--- src/utils/parsers/golang/index.ts
+++ src/utils/parsers/golang/index.ts
@@ -1,15 +1,13 @@
 import { BaseParser } from '../BaseParser'
 
-const authPattern = /\w*/
 const userCapturePattern = /(?:\w*|[\w\s.]*{[^})]*})/
 const variableNameCapturePattern = /([^,)]*)/
 const defaultValueCapturePattern = /(?:[^,)]*)/
 
 export class GolangParser extends BaseParser {
     identity = 'golang'
-    variableMethodPattern = /\.DevCycleApi\.Variable\(\s*/
+    variableMethodPattern = /\.Variable\(\s*/
     orderedParameterPatterns = [
-        authPattern,
         userCapturePattern,
         variableNameCapturePattern,
         defaultValueCapturePattern
