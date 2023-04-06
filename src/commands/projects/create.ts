cb7ef3ffb7ae2af6c50c8a7a7c99d5472af6bd6b
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2023-04-05 20:08:54 -0400

    feat: create project

---
+++ src/commands/projects/create.ts
@@ -0,0 +1,16 @@
+import { createProject, CreateProjectParams } from '../../api/projects'
+import { descriptionPrompt, keyPrompt, namePrompt } from '../../ui/prompts'
+import CreateCommand from '../createCommand'
+
+export default class CreateProject extends CreateCommand<CreateProjectParams> {
+    static hidden = false
+    static description = 'Create a new Project'
+
+    prompts = [keyPrompt, namePrompt, descriptionPrompt]
+
+    public async run(): Promise<void> {
+        const params = await this.populateParameters(CreateProjectParams, false)
+        const result = await createProject(this.token, params)
+        this.writer.showResults(result)
+    }
+}
