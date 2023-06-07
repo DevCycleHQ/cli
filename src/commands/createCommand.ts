b88f7dfe28f46fb13f34fd28f3cee16254ba24b7
Parth Suthar <parth.suthar@taplytics.com>
2023-06-07 10:29:12 -0400

    [chore] add headless functionality to env create command

--- src/commands/createCommand.ts
+++ src/commands/createCommand.ts
@@ -1,22 +1,51 @@
+import { Flags } from '@oclif/core'
 import { ClassConstructor, plainToClass } from 'class-transformer'
 import { validateSync } from 'class-validator'
 import inquirer from 'inquirer'
 import { reportValidationErrors } from '../utils/reportValidationErrors'
 import Base from './base'
 
+type prompts = {
+    name: string; message: string; type: string;
+}
+
 export default abstract class CreateCommand<ResourceType> extends Base {
-    prompts: inquirer.QuestionCollection = []
+    prompts: prompts[] = []
     authRequired = true
+
+    static flags = {
+        ...Base.flags,
+        'key': Flags.string({
+            description: 'Unique ID to refer to the environment.'
+        }),
+        'name': Flags.string({
+            description: 'Human readable name of the environment.',
+        }),
+        'description': Flags.string({
+            description: 'Description for display in the dashboard.',
+        }),
+    }
 
     public async populateParameters(
         paramClass: ClassConstructor<ResourceType>,
-        requireProject = true
+        requireProject = true,
+        flags: Record<string, unknown> = {},
     ): Promise<ResourceType> {
+        this.filterPrompts(flags)
         if (requireProject) {
             await this.requireProject()
         }
+        if (flags.headless) {
+            const params = plainToClass(paramClass, flags)
+            const errors = validateSync(params as Record<string, unknown>, {
+                whitelist: true,
+            })
+            reportValidationErrors(paramClass.name, errors)
+            return params
+        }
         const answers = await this.populateParametersWithInquirer()
-        const params = plainToClass(paramClass, answers)
+
+        const params = plainToClass(paramClass, this.mergeFlagsAndAnswers(flags, answers))
         const errors = validateSync(params as Record<string, unknown>, {
             whitelist: true,
         })
@@ -29,5 +58,22 @@ export default abstract class CreateCommand<ResourceType> extends Base {
             token: this.authToken,
             projectKey: this.projectKey,
         })
+    }
+
+    private async filterPrompts(flags: Record<string, unknown>) {
+        Object.keys(flags).forEach((key) => {
+            if (flags[key]) {
+                this.prompts = this.prompts.filter((prompt) => prompt.name !== key)
+            }
+        })
+    }
+
+    private mergeFlagsAndAnswers(flags: Record<string, unknown>, answers: Record<string, unknown>) {
+        Object.keys(flags).forEach((key) => {
+            if (flags[key] === undefined) {
+                flags[key] = answers[key]
+            }
+        })
+        return flags
     }
 }
