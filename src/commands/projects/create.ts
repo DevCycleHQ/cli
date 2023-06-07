8417c7c5a2c5b50d25539c9cf28e45f62bdf8215
Parth Suthar <parth.suthar@taplytics.com>
2023-06-07 10:29:41 -0400

    [DVC-7618] [chore] add headless functionality to prohects create command

--- src/commands/projects/create.ts
+++ src/commands/projects/create.ts
@@ -9,7 +9,18 @@ export default class CreateProject extends CreateCommand<CreateProjectParams> {
     prompts = [keyPrompt, namePrompt, descriptionPrompt]
 
     public async run(): Promise<void> {
-        const params = await this.populateParameters(CreateProjectParams, false)
+        const { flags } = await this.parse(CreateProject)
+        const { headless, key, name, description } = flags
+        if (headless && (!key || !name)) {
+            this.writer.showError('In headless mode, the key and name flags are required')
+            return
+        }
+        const params = await this.populateParameters(CreateProjectParams, false, {
+            key,
+            name,
+            description,
+            headless
+        })
         const result = await createProject(this.authToken, params)
         this.writer.showResults(result)
     }
