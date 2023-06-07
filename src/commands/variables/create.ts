074eaee6d16febf22de0723bd23e584e56599c31
Parth Suthar <parth.suthar@taplytics.com>
2023-06-07 10:29:58 -0400

    [DVC-7619] [chore] add headless functionality to variables create command

--- src/commands/variables/create.ts
+++ src/commands/variables/create.ts
@@ -1,6 +1,8 @@
+import { Flags } from '@oclif/core'
 import {
     createVariable,
-    CreateVariableParams
+    CreateVariableParams,
+    variableTypes
 } from '../../api/variables'
 import {
     descriptionPrompt,
@@ -15,6 +17,17 @@ export default class CreateVariable extends CreateCommand<CreateVariableParams> 
     static hidden = false
     static description = 'Create a new Variable for an existing Feature.'
 
+    static flags = {
+        ...CreateCommand.flags,
+        'type': Flags.string({
+            description: 'The type of variable',
+            options: variableTypes
+        }),
+        'feature': Flags.string({
+            description: 'The key or id of the feature to create the variable for'
+        })
+    }
+
     prompts = [
         keyPrompt,
         namePrompt,
@@ -24,8 +37,23 @@ export default class CreateVariable extends CreateCommand<CreateVariableParams> 
     ]
 
     public async run(): Promise<void> {
+        const { flags } = await this.parse(CreateVariable)
+
+        const { key, name, description, type, feature, headless } = flags
+
+        if (headless && (!key || !name || !type)) {
+            this.writer.showError('In headless mode, the key, name and type are required')
+            return
+        }
         await this.requireProject()
-        const params = await this.populateParameters(CreateVariableParams)
+        const params = await this.populateParameters(CreateVariableParams, true, {
+            key,
+            name,
+            description,
+            type,
+            feature,
+            headless
+        })
         const result = await createVariable(this.authToken, this.projectKey, params)
         this.writer.showResults(result)
     }
