d6c7e10afa57620b3ff7ec3aa21daa3218c13317
Elliot Campbell <elliotgithub@gmail.com>
2023-06-21 13:39:44 -0400

    fix comments

--- src/api/variations.ts
+++ src/api/variations.ts
@@ -1,6 +1,9 @@
+import { IsString, IsOptional } from 'class-validator'
 import apiClient from './apiClient'
 import { CreateVariationParams, Feature, Variation } from './schemas'
+import { buildHeaders } from './common'
 
+const variationsUrl = '/v1/projects/:project/features/:feature/variations'
 export class UpdateVariationParams {
     @IsString()
     @IsOptional()
@@ -14,11 +17,8 @@ export class UpdateVariationParams {
     variables?: { [key: string]: string | number | boolean | Record<string, unknown> }
 }
 export const fetchVariations = async (token: string, project_id: string, feature_key: string): Promise<Variation[]> => {
-    const response = await apiClient.get('/v1/projects/:project/features/:feature/variations', {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: token,
-        },
+    const response = await apiClient.get(variationsUrl, {
+        headers: buildHeaders(token),
         params: {
             project: project_id,
             feature: feature_key,
@@ -34,7 +34,7 @@ export const fetchVariationByKey = async (
     feature_key: string,
     variationKey: string
 ): Promise<Variation> => {
-    const response = await apiClient.get('/v1/projects/:project/features/:feature/variations/:key', {
+    const response = await apiClient.get(`${variationsUrl}/:key`, {
         headers: {
             'Content-Type': 'application/json',
             Authorization: token,
