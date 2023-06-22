e527e9b51dd780bee47c6799cdf89ebe921f73cd
Elliot Campbell <elliotgithub@gmail.com>
2023-06-22 15:07:32 -0400

    add headless checks to variations get/list

--- src/commands/variations/list.ts
+++ src/commands/variations/list.ts
@@ -18,10 +18,13 @@ export default class ListVariations extends Base {
 
     public async run(): Promise<void> {
         await this.requireProject()
-        const { args } = await this.parse(ListVariations)
-
+        const { args, flags } = await this.parse(ListVariations)
+        const { headless } = flags
         let featureKey
-        if (!args.feature) {
+        if (headless && !args.feature) {
+            this.writer.showError('In headless mode, feature is required')
+            return
+        } else if (!args.feature) {
             const { feature } = await inquirer.prompt([featurePrompt], {
                 token: this.authToken,
                 projectKey: this.projectKey
