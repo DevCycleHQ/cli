917ddf35966cbdb9b96cd132d6c833e20646adf9
Nika Salamadze <nika.salamadze@taplytics.com>
2023-07-07 14:24:21 -0400

    chore: add features delete and list description

--- src/commands/features/list.ts
+++ src/commands/features/list.ts
@@ -3,6 +3,7 @@ import Base from '../base'
 
 export default class ListFeatures extends Base {
     static hidden = false
+    static description = 'View all features in a project'
     authRequired = true
 
     public async run(): Promise<void> {
