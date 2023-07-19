72832a4df33a64ebc2295161b823652683798cf9
Jason Salaber <jcsalaber@hotmail.com>
2023-07-19 15:15:51 -0400

    feat: add flag to usages to only list variable usages that arent a variable in the api

--- src/commands/usages/usages.test.ts
+++ src/commands/usages/usages.test.ts
@@ -2,15 +2,49 @@ import { expect, test } from '@oclif/test'
 import chai from 'chai'
 import { jestSnapshotPlugin } from 'mocha-chai-jest-snapshot'
 import { setCurrentTestFile } from '../../../test-utils'
+import { BASE_URL } from '../../api/common'
 
 describe('usages', () => {
     beforeEach(setCurrentTestFile(__filename))
     chai.use(jestSnapshotPlugin())
+    
+    const projectKey = 'test-project'
+    const mockVariable = {
+        '_id': '648a0d55c4e88cd4c4544c58',
+        '_project': '63b5ee5de6e91987bae47f01',
+        '_feature': '646f8bb69302b0862fd68a39',
+        'name': 'SPAM SPAM SPAM',
+        'key': 'variable-from-api',
+        'description': 'spammity spam ',
+        'type': 'String',
+        'status': 'active',
+        'source': 'cli',
+        '_createdBy': 'google-oauth2|111559006563333334214',
+        'createdAt': '2023-06-14T18:56:21.270Z',
+        'updatedAt': '2023-06-14T18:56:21.270Z'
+    }
 
     test
         .stdout()
         .command(['usages', '--include', 'test-utils/fixtures/usages/nodejs.js'])
         .it('runs against a node test file', (ctx) => {
+            expect(ctx.stdout).toMatchSnapshot()
+        })
+    
+    test
+        .nock(BASE_URL, (api) => api
+            .get(`/v1/projects/${projectKey}/variables?perPage=1000&page=1&status=active`)
+            .reply(200, [mockVariable])
+        ).stdout()
+        .command([
+            'usages', 
+            '--only-unused', 
+            '--include', 
+            'test-utils/fixtures/usages/nodejs.js', 
+            '--project',
+            projectKey,
+        ])
+        .it('runs against a node test file and only returns variables not found as a variable in the api', (ctx) => {
             expect(ctx.stdout).toMatchSnapshot()
         })
 
