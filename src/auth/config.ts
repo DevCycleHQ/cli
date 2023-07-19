608af95a126214b6f390d92675813ae0530b7fc8
Laura Warr <laura.warr@taplytics.com>
2023-07-19 09:24:17 -0700

    feat: cache token to reuse if client id and secret match

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -33,7 +33,7 @@ export class AuthConfig {
     sso?: SSOAuthConfig
 }
 
-export function storeAccessToken(accessToken:string, authPath:string): void {
+export function storeAccessToken(accessToken: string, authPath: string): void {
     const configDir = path.dirname(authPath)
     if (!fs.existsSync(configDir)) {
         fs.mkdirSync(configDir, { recursive: true })
