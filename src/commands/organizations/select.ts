eb6299304c150bb75cac7a0c8b6f4fa2ba101077
Laura Warr <laura.warr@taplytics.com>
2023-07-31 15:07:59 -0700

    BREAKING CHANGE: Rename 'dvc org' to 'dvc organizations select'

--- src/commands/organizations/select.ts
+++ src/commands/organizations/select.ts
@@ -1,4 +1,5 @@
 import AuthCommand from '../authCommand'
+
 export default class SelectOrganization extends AuthCommand {
     static description = 'Select which organization to access through the API'
     static hidden = false
