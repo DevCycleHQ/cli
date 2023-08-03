27ad07d3a940bdcedcf399d2dc15e77ef6a80b9b
Max Nowack <maxnowack@users.noreply.github.com>
2023-08-03 12:35:30 +0200

    fix: exclude context lines from git diff

--- src/utils/diff/diff.ts
+++ src/utils/diff/diff.ts
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs'
 
 export const executeDiff = (diffCommand: string): parse.File[] => {
     try {
-        execSync(`git diff ${diffCommand} > diff.txt`, { stdio: 'ignore' })
+        execSync(`git diff --unified=0 ${diffCommand} > diff.txt`, { stdio: 'ignore' })
         return parse(readFileSync('diff.txt', 'utf8'))
     } finally {
         execSync('rm diff.txt', { stdio: 'ignore' })
