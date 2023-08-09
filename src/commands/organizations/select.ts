1bc421ee5d140d7f283adf587c127428ed11324f
Laura Warr <laura.warr@taplytics.com>
2023-08-09 11:08:53 -0700

    fix: Update org select command to require user auth

--- src/commands/organizations/select.ts
+++ src/commands/organizations/select.ts
@@ -3,7 +3,7 @@ import AuthCommand from '../authCommand'
 export default class SelectOrganization extends AuthCommand {
     static description = 'Select which organization to access through the API'
     static hidden = false
-    authRequired = true
+    userAuthRequired = true
 
     public async run(): Promise<void> {
         await this.setOrganizationAndProject()
