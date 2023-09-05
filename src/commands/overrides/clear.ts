e7b9a6d2ead3ba7868c5e98765be392ebab0ec8a
Chantal Chung <chantal.chung@devcycle.com>
2023-09-05 16:57:32 -0700

    feat: add dvc overrides clear for feature and environment

--- src/commands/overrides/clear.ts
+++ src/commands/overrides/clear.ts
@@ -1,13 +1,14 @@
 import inquirer from 'inquirer'
 
 import { Flags } from '@oclif/core'
-import { deleteAllProjectOverrides } from '../../api/overrides'
+import { deleteAllProjectOverrides, deleteFeatureOverrides } from '../../api/overrides'
 import Base from '../base'
+import { FeaturePromptResult, featurePrompt, EnvironmentPromptResult, environmentPrompt } from '../../ui/prompts'
 
 export default class DeleteOverrides extends Base {
     static hidden = false
     authRequired = true
-    static description = 'Clear Overrides for a given Project.'
+    static description = 'Clear Overrides for a given Feature or Project.'
 
     prompts = []
 
@@ -19,35 +20,79 @@ export default class DeleteOverrides extends Base {
             description: 'All Overrides for the Project',
             allowNo: false,
         }),
+        feature: Flags.string({
+            name: 'feature',
+            description: 'The key or id of the Feature to clear the Override for',
+        }),
+        environment: Flags.string({
+            name: 'environment',
+            description: 'The key or id of the Environment to clear the Override for',
+        }),
         ...Base.flags,
     }
 
     public async run(): Promise<void> {
 
         const { flags } = await this.parse(DeleteOverrides)
-        const { headless, project } = flags
+        const { headless, project, all, feature, environment } = flags
 
-        if (!flags.all) {
-            this.writer.showError('The \'--all\' flag is required to proceed.')
-            return
-        }
         await this.requireProject(project, headless)
 
-        if (!headless) {
-            const { confirmClear } = await inquirer.prompt([{
-                name: 'confirmClear',
-                message: `Are you sure you want to clear ALL Overrides for project: ${this.projectKey}?`,
-                type: 'confirm'
-            }])
-            if (!confirmClear) {
-                this.writer.warningMessage(`No Overrides cleared for project: ${this.projectKey}`)
-                return
+        if (all) {
+            if (!headless) {
+                const { confirmClear } = await inquirer.prompt([{
+                    name: 'confirmClear',
+                    message: `Are you sure you want to clear ALL Overrides for project: ${this.projectKey}?`,
+                    type: 'confirm'
+                }])
+                if (!confirmClear) {
+                    this.writer.warningMessage(`No Overrides cleared for project: ${this.projectKey}`)
+                    return
+                }
             }
+
+            await deleteAllProjectOverrides(this.authToken, this.projectKey)
+            this.writer.successMessage(
+                `Successfully cleared all overrides for project: ${this.projectKey}`
+            )
+            return
         }
 
-        await deleteAllProjectOverrides(this.authToken, this.projectKey)
+        let featureKey = feature
+        if (!feature && !headless) {
+            const featurePromptResult = await inquirer.prompt<FeaturePromptResult>([featurePrompt], {
+                token: this.authToken,
+                projectKey: this.projectKey
+            })
+            featureKey = featurePromptResult.feature.key
+        }
+
+        let environmentKey = environment
+        if (!environment && !headless) {
+            const { environment: environmentPromptResult } = await inquirer.prompt<EnvironmentPromptResult>(
+                [environmentPrompt], {
+                    token: this.authToken,
+                    projectKey: this.projectKey
+                })
+            environmentKey = environmentPromptResult._id
+        }
+
+        if (!featureKey || !environmentKey) {
+            this.writer.showError(
+                'Both the \'--feature\' and \'--environment\' flags or the \'--all\' flag must be provided to proceed.'
+            )
+            return
+        }
+                
+        await deleteFeatureOverrides(
+            this.authToken,
+            this.projectKey,
+            featureKey,
+            environmentKey
+        )
         this.writer.successMessage(
-            `Successfully cleared all overrides for project: ${this.projectKey}`
+            `Successfully cleared all overrides for the Feature '${featureKey}'`
+                    + ` and Environment '${environmentKey}'`
         )
     }
 }
