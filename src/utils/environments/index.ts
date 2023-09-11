e5e09ab258780a14e52d00378659f335a4d74d67
Kaushal Kapasi <91074031+kaushalkapasi@users.noreply.github.com>
2023-09-11 16:23:48 -0400

    DVC-8827 [fix]: adds defaults sort by feature name then by environment type display order (dev, staging, prod) (#287)

---
+++ src/utils/environments/index.ts
@@ -0,0 +1,19 @@
+import { Environment } from '../../api/schemas'
+
+const environmentsDisplayOrder = [
+    'development', 'staging', 'production', 'disaster_recovery'
+]
+
+export const getDisplayOrderedEnvironmentList = (
+    environments: Environment[],
+): string[] => {
+    return environments.sort((a, b) => {
+        if (a.type === b.type) {
+            return a.name.localeCompare(b.name)
+        }
+        return (
+            environmentsDisplayOrder.indexOf(a.type) -
+            environmentsDisplayOrder.indexOf(b.type)
+        )
+    }).map((env) => env._id)
+}
