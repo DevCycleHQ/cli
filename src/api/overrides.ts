90079acb83ae96de531b31698f76ba1e5b92de42
Parth Suthar <parth.suthar@taplytics.com>
2023-09-11 12:20:10 -0400

    chore: filter prompts to only overrides related information

--- src/api/overrides.ts
+++ src/api/overrides.ts
@@ -57,18 +57,17 @@ export const fetchFeatureOverridesForUser = async (
     token: string,
     project_id: string,
     feature_id: string,
-    environment_id: string
+    environment_id?: string
 ) => {
     const endpoint = '/features/:feature/overrides/current'
+    const queries = environment_id ? { environment: environment_id } : {}
     return apiClient.get(`${BASE_URL}${endpoint}`, {
         headers: buildHeaders(token),
         params: {
             project: project_id,
             feature: feature_id,
         },
-        queries: {
-            environment: environment_id,
-        },
+        queries
     })
 }
 
--- src/utils/variations/index.ts
+++
@@ -1,9 +0,0 @@
-import { fetchVariables } from '../../api/variables'
-import {
-    variableValueBooleanPrompt,
-    variableValueJSONPrompt,
-    variableValueNumberPrompt,
-    variableValueStringPrompt
-} from '../../ui/prompts'
-import inquirer, { ListQuestion, Question } from 'inquirer'
-
