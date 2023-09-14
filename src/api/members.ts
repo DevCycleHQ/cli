a91680975a6e6db4563640bd2505d570d7515567
Nika Salamadze <nika.salamadze@taplytics.com>
2023-09-14 10:02:12 -0400

    feat: add info comments to type generator

---
+++ src/api/members.ts
@@ -0,0 +1,19 @@
+import { axiosClient } from './apiClient'
+
+export type OrganizationMember = {
+  user_id?: string | undefined;
+  picture?: string | undefined;
+  name?: string | undefined;
+  email?: string | undefined;
+}
+
+export const fetchOrganizationMembers = async (token: string): Promise<OrganizationMember[]> => {
+    const response = await axiosClient.get('/v1/organizations/current/members', {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+        },
+    })
+
+    return response.data
+}
