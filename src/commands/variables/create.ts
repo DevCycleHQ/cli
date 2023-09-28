1b6bda06a7982256741f27486991a90e130653e8
Elliot Campbell <elliotgithub@gmail.com>
2023-09-28 16:42:08 -0400

    wip

--- src/commands/variables/create.ts
+++ src/commands/variables/create.ts
@@ -3,9 +3,11 @@ import {
     createVariable,
 } from '../../api/variables'
 import CreateCommand from '../createCommand'
-import { createVariablePrompts, variableFeaturePrompt } from '../../ui/prompts'
-import { CreateVariableDto } from '../../api/schemas'
+import { createVariablePrompts, featurePrompt } from '../../ui/prompts'
+import {CreateVariableDto, UpdateFeatureDto, Variable} from '../../api/schemas'
 import inquirer from '../../ui/autocomplete'
+import { VariableListOptions } from "../../ui/prompts/listPrompts/variablesListPrompt"
+import {updateFeature} from "../../api/features";
 
 export default class CreateVariable extends CreateCommand {
     static hidden = false
@@ -20,6 +22,9 @@ export default class CreateVariable extends CreateCommand {
         'feature': Flags.string({
             description: 'The key or id of the feature to create the variable for'
         }),
+        'variations': Flags.string({
+            description: 'Set a value for this variable in each variation of the associated feature'
+        }),
         'description': Flags.string({
             description: 'Description for the dashboard',
         }),
@@ -29,11 +34,15 @@ export default class CreateVariable extends CreateCommand {
 
     public async run(): Promise<void> {
         const { flags } = await this.parse(CreateVariable)
-        const { key, name, type, feature, headless, project } = flags
+        const { key, name, type, feature, headless, project, variations } = flags
         await this.requireProject(project, headless)
 
         if (headless && (!key || !name || !type || !feature)) {
             this.writer.showError('The key, name, feature, and type flags are required')
+            return
+        }
+        if (headless && variations && !feature) {
+            this.writer.showError('Cannot modify variations without associating to a feature')
             return
         }
         flags._feature = feature
@@ -46,9 +55,28 @@ export default class CreateVariable extends CreateCommand {
                 type: 'confirm',
                 default: false
             }])
-    
+
             if (associateToFeature) {
-                params = await this.populateParametersWithZod(CreateVariableDto, [variableFeaturePrompt], params)
+                const { feature } = await inquirer.prompt([featurePrompt])
+                if (!feature) {
+                    this.writer.showError(`Feature with key ${feature.key} could not be found`)
+                    return
+                }
+                params = await this.populateParametersWithZod(CreateVariableDto, [], params)
+                const variableListOptions = new VariableListOptions(
+                    [params as Variable],
+                    this.writer,
+                    !variations ? feature.variations: undefined // don't prompt for variations if flag provided
+                )
+                await variableListOptions.promptVariationValues(params as Variable)
+                const featureParams = await this.populateParametersWithZod(UpdateFeatureDto, this.prompts, {
+                    key: feature.key,
+                    name: feature.name,
+                    variations: JSON.parse(feature.ariations),
+                    headless,
+                })
+                await updateFeature(this.authToken, this.projectKey, feature.key, featureParams)
+
             }
         }
         const result = await createVariable(this.authToken, this.projectKey, params)
