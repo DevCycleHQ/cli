f00ea7391cf7fab09c1e9e1a1b54ad3b0769141a
Emma Willis <ehenriks@uwo.ca>
2023-10-02 14:27:28 -0400

    Add includeFiles and excludeFiles to diff cmd

--- src/utils/diff/diff.ts
+++ src/utils/diff/diff.ts
@@ -1,10 +1,21 @@
 import { execSync } from 'node:child_process'
 import parse from 'parse-diff'
 import { readFileSync } from 'fs'
+import { getFilteredFiles } from '../getFilteredFiles'
+import { CodeInsights } from '../../types'
 
-export const executeDiff = (diffCommand: string): parse.File[] => {
+export const executeDiff = (
+    diffCommand: string, 
+    flags: { include?: string[], exclude?: string[] },
+    codeInsightsConfig?: CodeInsights
+): parse.File[] => {
     try {
-        execSync(`git diff ${diffCommand} > diff.txt`, { stdio: 'ignore' })
+        let files = ''
+        if (codeInsightsConfig?.includeFiles || codeInsightsConfig?.excludeFiles || flags.include || flags.exclude) {
+            files = ' ' + getFilteredFiles(flags, codeInsightsConfig).join(' ')
+        }
+
+        execSync(`git diff ${diffCommand}${files} > diff.txt`, { stdio: 'ignore' })
         return parse(readFileSync('diff.txt', 'utf8'))
     } finally {
         execSync('rm diff.txt', { stdio: 'ignore' })
