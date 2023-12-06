41258edf279561e7f2b867cf51351d72b119c9cb
Nika Salamadze <nika.salamadze@taplytics.com>
2023-12-06 11:44:21 -0500

    fix: usages command not working for untracked files

--- src/utils/git/ls-files.ts
+++ src/utils/git/ls-files.ts
@@ -1,7 +1,9 @@
 import { execSync } from 'node:child_process'
 
 export const lsFiles = (): string[] => {
-    const fileBuffer = execSync('git ls-files')
+    // --others & --exclude-standard: get untracked files that are not ignored
+    // --cached: get files that are staged
+    const fileBuffer = execSync('git ls-files --others --cached --exclude-standard')
     const files = fileBuffer.toString().split('\n').filter(Boolean)
 
     return files
