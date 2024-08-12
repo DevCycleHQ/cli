da2c188004451a147ac6198f07d925d86bcafe4a
Kaushal Kapasi <91074031+kaushalkapasi@users.noreply.github.com>
2024-08-12 10:08:12 -0400

    feat: add --no-browser-auth option for authenticating via the CLI (#417)
    feat: add --no-browser-auth option for authenticating via the CLI in environments that cannot open a browser

--- src/commands/login/sso.ts
+++ src/commands/login/sso.ts
@@ -10,8 +10,11 @@ export default class LoginSSO extends AuthCommand {
     static examples = []
 
     public async run(): Promise<void> {
+        const { flags } = await this.parse(LoginSSO)
+        const noBrowser = flags['no-browser-auth']
+
         const ssoAuth = new SSOAuth(this.writer, this.authPath)
-        const tokens = await ssoAuth.getAccessToken()
+        const tokens = await ssoAuth.getAccessToken(noBrowser)
         this.authToken = tokens.accessToken
         this.personalAccessToken = tokens.personalAccessToken
         await this.setOrganizationAndProject()
