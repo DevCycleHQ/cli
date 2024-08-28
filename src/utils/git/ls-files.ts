9d9c2a893e9ae618bbe00c384c21675fd942ab71
Parth Suthar <parth.suthar@taplytics.com>
2024-08-28 15:29:08 -0400

    fix: update buffer size to 10MB (#427)

--- src/utils/git/ls-files.ts
+++ src/utils/git/ls-files.ts
@@ -4,7 +4,8 @@ export const lsFiles = (): string[] => {
     // --others & --exclude-standard: get untracked files that are not ignored
     // --cached: get files that are staged
     const fileBuffer = execSync(
-        'git ls-files --others --cached --exclude-standard',
+        'git ls-files --others --cached --exclude-standard ',
+        { maxBuffer: 1024 * 1024 * 10 }, // 10 MB buffer
     )
     const files = fileBuffer.toString().split('\n').filter(Boolean)
 
