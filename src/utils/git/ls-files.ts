cee328d188f15252b13f22795a82e56ba0ec2e01
Parth Suthar <parth.suthar@taplytics.com>
2024-09-05 10:07:53 -0400

    chore: add error message if ls-files command fails (#428)

--- src/utils/git/ls-files.ts
+++ src/utils/git/ls-files.ts
@@ -1,13 +1,22 @@
 import { execSync } from 'node:child_process'
 
 export const lsFiles = (): string[] => {
-    // --others & --exclude-standard: get untracked files that are not ignored
-    // --cached: get files that are staged
-    const fileBuffer = execSync(
-        'git ls-files --others --cached --exclude-standard ',
-        { maxBuffer: 1024 * 1024 * 10 }, // 10 MB buffer
-    )
-    const files = fileBuffer.toString().split('\n').filter(Boolean)
+    try {
+        // --others & --exclude-standard: get untracked files that are not ignored
+        // --cached: get files that are staged
+        const fileBuffer = execSync(
+            'git ls-files --others --cached --exclude-standard ',
+            { maxBuffer: 1024 * 1024 * 10 }, // 10 MB buffer
+        )
+        const files = fileBuffer.toString().split('\n').filter(Boolean)
 
-    return files
+        return files
+    } catch (error) {
+        if (error instanceof Error && error.message.includes('ENOBUFS')) {
+            console.warn(
+                'This directory has too many files to process. Contact DevCycle support.',
+            )
+        }
+        return []
+    }
 }
