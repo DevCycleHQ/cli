91de0a6291173248d8d73a64b8443aeb51527bdd
Jonathan Norris <jonathan@taplytics.com>
2025-07-14 16:50:48 -0400

    fix: generate-zodios-client.sh script

--- scripts/generate-zodios-client.sh
+++ scripts/generate-zodios-client.sh
@@ -2,5 +2,5 @@
 url="https://api.devcycle.com/swagger.json"
 temp_file=$(mktemp)
 curl -s "$url" -o "$temp_file"
-openapi-zod-client "$temp_file" --with-alias --export-schemas --prettier "prettier.config.js" -o "src/api/zodClient.ts"
+yarn openapi-zod-client "$temp_file" --with-alias --export-schemas --prettier "prettier.config.js" -o "src/api/zodClient.ts"
 rm "$temp_file"
